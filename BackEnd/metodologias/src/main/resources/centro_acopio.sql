de474293e1d768163f443bbc2946ed44ef27d56b
Leonel Dimasi <ldimasi@alumnos.exa.unicen.edu.ar>
2021-05-17 17:54:50 -0300

    GMT-39 tabla de materiales db

---
+++ BackEnd/metodologias/src/main/resources/centro_acopio.sql
@@ -0,0 +1,33 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Base de datos: `centro_de_acopio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `material`
+--
+
+CREATE TABLE `material` (
+  `id_material` int(11) NOT NULL,
+  `categoria` varchar(60) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `imagen` text NOT NULL,
+  `condiciones` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`id_material`);
+
+
+ALTER TABLE `material`
+  MODIFY `id_material` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
