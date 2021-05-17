c62ded55023bd0b2e9629b2f039cbb87dd6d02c7
Leonel Dimasi <leoneldimasi@gmail.com>
2021-05-17 16:27:58 -0300

    GMT-49 Inicializar backend

---
+++ BackEnd/metodologias/src/test/java/com/metodologias/metodologias/CentroDeAcopioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metodologias.metodologias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroDeAcopioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
