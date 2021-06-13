6870bec54a9af795d1d932a0bf284e38e88caea7
Leonel Dimasi <ldimasi@alumnos.exa.unicen.edu.ar>
2021-06-13 16:18:26 -0300

    #37 abm-materiales avances servicio registrar material

--- BackEnd/metodologias/src/main/java/com/metodologias/metodologias/controller/MaterialController.java
+++ BackEnd/metodologias/src/main/java/com/metodologias/metodologias/controller/MaterialController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,4 +37,17 @@ public class MaterialController {
 		}
 	}
 
+	@PostMapping("")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 500, message = "Internal Server Error")
+	})
+	public @ResponseBody ResponseEntity<MaterialDTO> insertUser(@RequestBody MaterialDTO material ){
+		try {	
+			return new ResponseEntity<MaterialDTO>(materialService.insertMaterial(material),HttpStatus.OK);
+		}catch(Exception e) {
+			LOGGER.error(INTERNAL_SERVER_ERROR,e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
