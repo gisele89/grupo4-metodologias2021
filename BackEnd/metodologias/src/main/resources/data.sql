3e62eeadc4810505913c2d5b02c2ab71fbe8b371
Leonel Dimasi <ldimasi@alumnos.exa.unicen.edu.ar>
2021-06-13 21:31:36 -0300

    GMT-47 FIX SCRIPT BASE DE DATOS

--- BackEnd/metodologias/src/main/resources/data.sql
+++ BackEnd/metodologias/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO material (nombre,imagen,condiciones) VALUES ('aluminio','https://www.amarilloverdeyazul.com/wp-content/uploads/2010/09/papel-de-aluminio-2.jpg','blablabla');
-INSERT INTO material (nombre,imagen,condiciones) VALUES ('cobre','https://www.foremex.com.mx/blog/img/post/mejor-pagados.jpg','blablabla');
+INSERT INTO material (nombre,img,descripcion) VALUES ('aluminio','https://www.amarilloverdeyazul.com/wp-content/uploads/2010/09/papel-de-aluminio-2.jpg','blablabla');
+INSERT INTO material (nombre,img,descripcion) VALUES ('cobre','https://www.foremex.com.mx/blog/img/post/mejor-pagados.jpg','blablabla');
 
 
 
